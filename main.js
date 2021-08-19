f61cb862ab75ad81d3d97d075f8493044b88fe1e
SamruddhiDudam <78253578+SamruddhiDudam@users.noreply.github.com>
2021-08-19 18:13:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function preload() {
   }
   
 function setup() {
-  canvas = createCanvas(640,480);
+  canvas = createCanvas(300,300);
   canvas.center();
   video = createCapture(VIDEO);
-  video.size(640,480);
+  video.size(300,300);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
